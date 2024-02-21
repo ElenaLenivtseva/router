0b3e3d43cd2b8016237b25afe948f6fb14bcc6b8
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-21 19:23:30 +0700

    Реализован роутинг на десктопе. Добавлены стили для nav, активной ссылки и пр.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Link to="/" className="title">Website</Link>
+        
+      <ul>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Services</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
