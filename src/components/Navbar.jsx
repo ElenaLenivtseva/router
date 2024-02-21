69c5c3834da8cd4136d377ae83e46548d13bf761
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-21 19:58:13 +0700

    Добавлена логика мобильного меню, в т.ч. закрывание меню после перехода по ссылке

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const closeMenuAfterClick =()=> {
+        if (menuOpen) {
+            setMenuOpen(!menuOpen)
+        }
+    }
   return (
     <nav>
-      <Link to="/" className="title">
+      <Link to="/" className="title" onClick={closeMenuAfterClick}>
         Website
       </Link>
-      <div className="menu">
+      <div className="menu" onClick={()=>setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul>
-        <li>
+      <ul className={menuOpen ? 'open' : ''}>
+        <li onClick={closeMenuAfterClick}>
           <NavLink to="/about">About</NavLink>
         </li>
-        <li>
+        <li onClick={closeMenuAfterClick}>
           <NavLink to="/services">Services</NavLink>
         </li>
-        <li>
+        <li onClick={closeMenuAfterClick}>
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
